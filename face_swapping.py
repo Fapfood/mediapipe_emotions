57aaec84b607003ef82f352764b54270c3b29726
matr@grapeup.com <matr@grapeup.com>
2022-05-10 11:37:07 +0200

    cleanup

--- face_swapping.py
+++ face_swapping.py
@@ -5,24 +5,23 @@ import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
-change_face_list = ["jim_carrey.jpg"]
-MAX_FACESWAP_IMAGES = len(change_face_list) - 1
+
+change_face = 'jim_carrey.jpg'
 
 
-def load_base_img(face_mesh, image_file_name, ):
+def load_base_img(face_mesh, image_file_name):
     image = cv2.imread(image_file_name)
-    results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    return {"img": image, "landmarks": results}
+    landmarks = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    return {'img': image, 'landmarks': landmarks}
 
 
 def transform_landmarks_from_tf_to_ocv(keypoints, face_width, face_height):
     landmark_list = []
-    if (keypoints.multi_face_landmarks != None):
+    if keypoints.multi_face_landmarks is not None:
         for face_landmarks in keypoints.multi_face_landmarks:
-            for l in face_landmarks.landmark:
+            for lm in face_landmarks.landmark:
                 # print(l.x, l.y, l.z)
-                pt = mp_drawing._normalized_to_pixel_coordinates(l.x, l.y,
-                                                                 face_width, face_height)
+                pt = mp_drawing._normalized_to_pixel_coordinates(lm.x, lm.y, face_width, face_height)
                 landmark_list.append(pt)
     return landmark_list
 
@@ -32,62 +31,67 @@ def init_windows(height, width):
     cv2.resizeWindow('input', height, width)
 
 
-def match_triangles(keypoints, triangle_indexes):
-    pass
-
-
 def draw_triangulated_mesh(ocv_keypoints, img):
-    for i in range(0, int(len(tmp.TRIANGULATION) / 3)):
-        points = [tmp.TRIANGULATION[i * 3], tmp.TRIANGULATION[i * 3 + 1], tmp.TRIANGULATION[i * 3 + 2]
-                  ]
-        result1 = ocv_keypoints[points[0]]
-        result2 = ocv_keypoints[points[1]]
-        result3 = ocv_keypoints[points[2]]
+    for i in range(len(tmp.TRIANGULATION) // 3):
+        result1 = ocv_keypoints[tmp.TRIANGULATION[i * 3]]
+        result2 = ocv_keypoints[tmp.TRIANGULATION[i * 3 + 1]]
+        result3 = ocv_keypoints[tmp.TRIANGULATION[i * 3 + 2]]
         cv2.line(img, result1, result2, 255)
         cv2.line(img, result2, result3, 255)
         cv2.line(img, result3, result1, 255)
     return img
 
 
+def process_base_face_mesh(drawing_spec,
+                           face_mesh,
+                           image_file,
+                           show_landmarks=False,
+                           show_triangulated_mesh=False):
+    base_face_handler = load_base_img(face_mesh, image_file)
+    base_input_image = base_face_handler['img'].copy()
+    image_rows, image_cols, _ = base_face_handler['img'].shape
+    landmark_base_ocv = transform_landmarks_from_tf_to_ocv(base_face_handler['landmarks'], image_cols, image_rows)
+    if show_landmarks:
+        mp_drawing.draw_landmarks(
+            image=base_face_handler['img'],
+            landmark_list=base_face_handler['landmarks'].multi_face_landmarks[0],
+            connections=mp_face_mesh.FACEMESH_TESSELATION,
+            landmark_drawing_spec=drawing_spec,
+            connection_drawing_spec=drawing_spec)
+    if show_triangulated_mesh:
+        base_face_handler['img'] = draw_triangulated_mesh(landmark_base_ocv, base_face_handler['img'])
+    return base_face_handler, landmark_base_ocv, base_input_image
+
+
 def main():
-    print("----------------shortcuts-----------")
-    print("1 -> show face landmarks")
-    print("2 -> show triangulated mesh")
-    print("c -> change face_swaping base image")
-    print("S or s: save images")
-    print("----------------shortcuts-----------")
-    triangle_indexes = tmp.TRIANGULATION
-    key_draw_landmarks = False
-    key_draw_mask = False
+    key_draw_landmarks = True
+    key_draw_mask = True
     flag_change_face = False
     video_writer = None
-    face_list_index = 0
+
     # For webcam input:
     init_windows(480, 640)
-    face_mesh = mp_face_mesh.FaceMesh(
-        min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
     base_face_handler, landmark_base_ocv, base_input_image = \
-        process_base_face_mesh(drawing_spec, face_mesh, change_face_list[face_list_index],
+        process_base_face_mesh(drawing_spec, face_mesh, change_face,
                                show_landmarks=key_draw_landmarks,
                                show_triangulated_mesh=key_draw_landmarks)
     cap = cv2.VideoCapture(0)
     while cap.isOpened():
         success, webcam_img = cap.read()
         if not success:
-            break
+            continue
         if flag_change_face:
             base_face_handler, landmark_base_ocv, base_input_image = \
-                process_base_face_mesh(drawing_spec, face_mesh, change_face_list[face_list_index],
+                process_base_face_mesh(drawing_spec, face_mesh, change_face,
                                        show_landmarks=key_draw_landmarks,
                                        show_triangulated_mesh=key_draw_landmarks)
             flag_change_face = False
         image_rows, image_cols, _ = webcam_img.shape
         webcam_img.flags.writeable = False
         results = face_mesh.process(webcam_img)
-        landmark_target_ocv = transform_landmarks_from_tf_to_ocv(results,
-                                                                 image_cols,
-                                                                 image_rows)
+        landmark_target_ocv = transform_landmarks_from_tf_to_ocv(results, image_cols, image_rows)
         # Draw the face mesh annotations on the image.
         webcam_img.flags.writeable = True
         image = webcam_img.copy()
@@ -116,7 +120,7 @@ def main():
                     convexhull2 = cv2.convexHull(points2)
                     process = True
                     if process == True:
-                        for i in range(0, int(len(tmp.TRIANGULATION) / 3)):
+                        for i in range(len(tmp.TRIANGULATION) // 3):
                             triangle_index = [tmp.TRIANGULATION[i * 3],
                                               tmp.TRIANGULATION[i * 3 + 1],
                                               tmp.TRIANGULATION[i * 3 + 2]]
@@ -178,51 +182,22 @@ def main():
 
                         (x, y, w, h) = cv2.boundingRect(convexhull2)
                         center_face2 = (int((x + x + w) / 2), int((y + y + h) / 2))
-                        seamlessclone = cv2.seamlessClone(result, seam_clone,
-                                                          img2_head_mask, center_face2, cv2.MIXED_CLONE)
+                        seamlessclone = cv2.seamlessClone(result, seam_clone, img2_head_mask, center_face2,
+                                                          cv2.MIXED_CLONE)
         cv2.imshow("input", base_face_handler["img"])
         cv2.imshow('MediaPipe FaceMesh', out_image)
         cv2.imshow('seam', result)
         cv2.imshow('seamless', seamlessclone)
         key = cv2.waitKey(5)
         if key == 27:
-            break;
+            break
         if key == 49:
             key_draw_landmarks = not key_draw_landmarks
         if key == 50:
             key_draw_mask = not key_draw_mask
-        if key == 99:
-            flag_change_face = not flag_change_face
-            face_list_index = face_list_index + 1
-            if face_list_index > MAX_FACESWAP_IMAGES:
-                face_list_index = 0
     face_mesh.close()
     cap.release()
     video_writer.release()
-
-
-def process_base_face_mesh(drawing_spec,
-                           face_mesh,
-                           image_file,
-                           show_landmarks=False,
-                           show_triangulated_mesh=False):
-    base_face_handler = load_base_img(face_mesh, image_file)
-    base_input_image = base_face_handler["img"].copy()
-    image_rows, image_cols, _ = base_face_handler["img"].shape
-    landmark_base_ocv = \
-        transform_landmarks_from_tf_to_ocv(base_face_handler["landmarks"],
-                                           image_cols, image_rows)
-    if show_landmarks:
-        mp_drawing.draw_landmarks(
-            image=base_face_handler["img"],
-            landmark_list=base_face_handler["landmarks"].multi_face_landmarks[0],
-            connections=mp_face_mesh.FACE_CONNECTIONS,
-            landmark_drawing_spec=drawing_spec,
-            connection_drawing_spec=drawing_spec)
-    if show_triangulated_mesh:
-        base_face_handler["img"] = \
-            draw_triangulated_mesh(landmark_base_ocv, base_face_handler["img"])
-    return base_face_handler, landmark_base_ocv, base_input_image
 
 
 if __name__ == "__main__":
